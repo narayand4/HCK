622f52899cd85e9104671e343f2f37d2e7148c77
CGTDev92 <CGTDev92@cgt.co.in>
2016-12-29 19:32:08 +0530

    Updated code.

--- src/application/Main.java
+++ src/application/Main.java
@@ -49,7 +49,7 @@ public class Main {
 	}*/
 	
 	public static void main(String[] args) {
-		q56();
+		fabunacci();
 	}
 	
 	public static void q1(){
@@ -1171,6 +1171,27 @@ public class Main {
     		list.add(a);
     	}    	
     }
+    
+    private static void fabunacci(){
+    	Scanner in = new Scanner(System.in);
+    	int n = in.nextInt();
+    	for(int i=1; i<=n; i++){
+    		System.out.print(fibonacciLoop(i)+" ");
+    	}
+    }
+    
+    public static int fibonacciLoop(int number) {
+		if (number == 1 || number == 2) {
+			return 1;
+		}
+		int fibo1 = 1, fibo2 = 1, fibonacci = 1;
+		for (int i = 3; i <= number; i++) {
+			fibonacci = fibo1 + fibo2; 
+			fibo1 = fibo2;
+			fibo2 = fibonacci; 
+		}
+		return fibonacci; 
+	}
 }
 
 class MyRegex{
